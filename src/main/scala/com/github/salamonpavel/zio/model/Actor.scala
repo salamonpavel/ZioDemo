c8e68b3fc2d1eff8cb4fb6c6c3e99c7f08c7db7f
Pavel Salamon <salamonpavel@gmail.com>
2023-11-25 20:10:40 +0100

    docs added

--- src/main/scala/com/github/salamonpavel/zio/model/Actor.scala
+++ src/main/scala/com/github/salamonpavel/zio/model/Actor.scala
@@ -2,8 +2,20 @@ package com.github.salamonpavel.zio.model
 
 import zio.json.{DeriveJsonEncoder, JsonEncoder}
 
+
+/** 
+ * A case class representing an actor.
+ *
+ * @param actorId The ID of the actor.
+ * @param firstName The first name of the actor.
+ * @param lastName The last name of the actor.
+ */
 case class Actor(actorId: Int, firstName: String, lastName: String)
 
 object Actor {
+  /**
+   * An implicit JsonEncoder for the Actor case class.
+   * This encoder is used to convert instances of Actor to JSON.
+   */
   implicit val encoder: JsonEncoder[Actor] = DeriveJsonEncoder.gen[Actor]
 }
--- src/main/scala/com/github/salamonpavel/zio/model/Movie.scala
+++ src/main/scala/com/github/salamonpavel/zio/model/Movie.scala
@@ -2,8 +2,19 @@ package com.github.salamonpavel.zio.model
 
 import zio.json.{DeriveJsonEncoder, JsonEncoder}
 
+/**
+ * A case class representing a movie.
+ *
+ * @param movieId The ID of the movie.
+ * @param movieName The name of the movie.
+ * @param movieLength The length of the movie.
+ */
 case class Movie(movieId: Int, movieName: String, movieLength: Int)
 
 object Movie {
+  /**
+   * An implicit JsonEncoder for the Movie case class.
+   * This encoder is used to convert instances of Movie to JSON.
+   */
   implicit val encoder: JsonEncoder[Movie] = DeriveJsonEncoder.gen[Movie]
 }
