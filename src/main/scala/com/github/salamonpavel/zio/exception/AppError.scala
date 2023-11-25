c3aed38d7166df6b3b6aba201c0cd6e76fa13d9a
Pavel Salamon <salamonpavel@gmail.com>
2023-11-25 20:09:08 +0100

    docs added

--- src/main/scala/com/github/salamonpavel/zio/exception/AppError.scala
+++ src/main/scala/com/github/salamonpavel/zio/exception/AppError.scala
@@ -1,6 +1,16 @@
 package com.github.salamonpavel.zio.exception
 
+/** 
+ * An object representing application-specific errors.
+ */
 sealed trait AppError extends Throwable
 
+/**
+ * An error indicating that a required parameter is missing.
+ */
 case class RequiredParameterMissingError(message: String) extends AppError
+
+/**
+ * An error indicating that a parameter is not in the expected format.
+ */
 case class ParameterNumberFormatError(message: String) extends AppError
