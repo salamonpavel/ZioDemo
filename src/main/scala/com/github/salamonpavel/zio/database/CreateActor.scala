058997ec51596b80596243ad39c360b57047c6b6
Pavel Salamon <salamonpavel@gmail.com>
2023-11-29 10:57:49 +0100

    changes related to fa-db and repositories

---
+++ src/main/scala/com/github/salamonpavel/zio/database/CreateActor.scala
@@ -0,0 +1,41 @@
+package com.github.salamonpavel.zio.database
+
+import com.github.salamonpavel.zio.model.CreateActorRequestBody
+import slick.jdbc.{GetResult, SQLActionBuilder}
+import za.co.absa.fadb.DBFunction._
+import za.co.absa.fadb.DBSchema
+import za.co.absa.fadb.slick.FaDbPostgresProfile.api._
+import za.co.absa.fadb.slick.{SlickFunctionWithStatusSupport, SlickPgEngine}
+import za.co.absa.fadb.status.handling.implementations.StandardStatusHandling
+import zio._
+
+/**
+ *  A class representing a function to create an actor.
+ */
+class CreateActor(implicit override val schema: DBSchema, val dbEngine: SlickPgEngine)
+  extends DBSingleResultFunction[CreateActorRequestBody, Unit, SlickPgEngine]
+    with SlickFunctionWithStatusSupport[CreateActorRequestBody, Unit]
+    with StandardStatusHandling {
+
+  override protected def sql(createActorRequestBody: CreateActorRequestBody): SQLActionBuilder = {
+    sql"""SELECT #$selectEntry
+            FROM #$functionName(
+              ${createActorRequestBody.firstName},
+              ${createActorRequestBody.lastName}
+            ) #$alias;"""
+  }
+
+  override protected def slickConverter: GetResult[Unit] = GetResult { _ => }
+}
+
+object CreateActor {
+
+  /**
+   *  A ZLayer that provides live implementation of CreateActor.
+   */
+  val live: ZLayer[PostgresDatabaseProvider, Nothing, CreateActor] = ZLayer {
+    for {
+      dbProvider <- ZIO.service[PostgresDatabaseProvider]
+    } yield new CreateActor()(ActorsSchema, dbProvider.dbEngine)
+  }
+}
---
+++ src/main/scala/com/github/salamonpavel/zio/database/GetActorById.scala
@@ -0,0 +1,42 @@
+package com.github.salamonpavel.zio.database
+
+import com.github.salamonpavel.zio.model.Actor
+import slick.jdbc.{GetResult, PositionedResult, SQLActionBuilder}
+import za.co.absa.fadb.DBFunction._
+import za.co.absa.fadb.DBSchema
+import za.co.absa.fadb.slick.FaDbPostgresProfile.api._
+import za.co.absa.fadb.slick.{SlickFunction, SlickPgEngine}
+import zio.{ZIO, ZLayer}
+
+/**
+ *  A class representing a function to get a movie by ID.
+ */
+class GetActorById(implicit override val schema: DBSchema, val dbEngine: SlickPgEngine)
+  extends DBOptionalResultFunction[Int, Actor, SlickPgEngine]
+    with SlickFunction[Int, Actor] {
+
+  override def fieldsToSelect: Seq[String] = super.fieldsToSelect ++ Seq("actor_id", "first_name", "last_name")
+
+  override protected def sql(values: Int): SQLActionBuilder = {
+    sql"""SELECT #$selectEntry FROM #$functionName($values) #$alias;"""
+  }
+
+  override protected def slickConverter: GetResult[Actor] = {
+    def converter(r: PositionedResult): Actor = {
+      Actor(r.<<, r.<<, r.<<)
+    }
+    GetResult(converter)
+  }
+}
+
+object GetActorById {
+
+  /**
+   *  A ZLayer that provides live implementation of GetMovieById.
+   */
+  val live: ZLayer[PostgresDatabaseProvider, Nothing, GetActorById] = ZLayer {
+    for {
+      dbProvider <- ZIO.service[PostgresDatabaseProvider]
+    } yield new GetActorById()(ActorsSchema, dbProvider.dbEngine)
+  }
+}
